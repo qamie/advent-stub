e4bc6547003d6280cff96b8beedb3bc8f5ee06a1
github-nua-cc <113679327+github-nua-cc@users.noreply.github.com>
2023-12-14 13:34:36 +0000

    Add files via upload

---
+++ messages.js
@@ -0,0 +1,27 @@
+// https://www.kitchencounterchronicle.com/wp-content/uploads/2018/12/Silly-Christmas-Jokes-for-Kids-1.pdf
+const messages = [
+    { title: "What is Santa’s favourite track and field event?", text: "North Pole-vaulting"},
+    { title: "Where do you findreindeer?", text: "It depends on where you leave them!"},
+    { title: "What kind of ball doesn’t bounce?", text: "A snowbal"},
+    { title: "What do you call Santa when he stops moving?", text: "Santa pause"},
+    { title: "What did the gingerbread man put on his bed?", text: "A cookie sheet"},
+    { title: "What do Santa’s elves learn at school?", text: "The elf-abet"},
+    { title: "What is red and white and blue at Christmas time?", text: "A sad candy cane"},
+    { title: "What comes at the end of Christmas Day?", text: "The letter 'Y'"},
+    { title: "What do you call an elf who sings?", text: "A wrapper"},
+    { title: "Why is it always cold at Christmas?", text: "Because it’s Decemberrr"},
+    { title: "How many gifts can Santa fit in an empty sack?", text: "One – because after that it isn’t empty anymore"},
+    { title: "Which Christmas carol do parents love the most?", text: "Silent Night"},
+    { title: "Who delivers Christmas presents to dogs?", text: "Santa Paws"},
+    { title: "What falls at the North Pole, but never gets hurt?", text: "Snow"},
+    { title: "Knock, knock Who’s there?", text: "Donut. Donut who? Donut open until Christmas!"},
+    { title: "What do you get when you cross a snowwoman and a vampire?", text: "Frost Bite"},
+    { title: "What kind of bird can write?", text: "A pen-guin?"},
+    { title: "What do you get when you cross Santa and a detective?", text: "Santa Clue"},
+    { title: "Who says 'oh, oh, oh'?", text: "Santa walking backwards"},
+    { title: "What do you call an old snowman?", text: "Water"},
+    { title: "What is white and red and goes up and down,up and down, up and down?", text: "Santa riding anelevator"},
+    { title: "What is the best Christmas gift ever?", text: "A broken drum, you can’t beat it!"},
+    { title: "What did the one snowman say to the other snowman?", text: "Do you smell carrots?"},
+    { title: "Knock, knock Who’s there?", text: "Mary. Mary Who? Merry Christmas!"}
+    ];
